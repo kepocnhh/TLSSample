71592e134028efa5083baf7e1bc31ab01f34d069
Stanley Wintergreen <kepocnhh@gmail.com>
2024-06-11 23:58:09 +0300

    0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = namespace
         minSdk = Version.Android.minSdk
         targetSdk = Version.Android.targetSdk
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.$versionCode"
     }
 
